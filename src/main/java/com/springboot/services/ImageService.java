310bf17274ae71a538421f9d5b163953de6e64df
서국화 <kookhwa836@gmail.com>
2024-01-10 11:10:51 +0900

    add transactional annotation

--- src/main/java/com/springboot/services/ImageService.java
+++ src/main/java/com/springboot/services/ImageService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -27,17 +28,19 @@ public class ImageService {
         return imageRepository.findAll(pageable).getContent();
     }
 
+    @Transactional
     public void createImages() throws IOException {
         ResponseEntity<String> response = new RestTemplate().getForEntity(IMAGE_URL, String.class);
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> map = mapper.readValue(response.getBody(), HashMap.class);
 
-        ArrayList<LinkedHashMap> images = (ArrayList)map.get("images_results");
+        List<LinkedHashMap> images = (ArrayList)map.get("images_results");
         List<Image> imageList = mapper.convertValue(images, new TypeReference<List<Image>>(){});
 
         imageRepository.saveAll(imageList);
     }
 
+    @Transactional
     public Image createImage(ImageRequest request){
         Image image = Image.builder()
                            .thumbnail(request.getThumbnail())
